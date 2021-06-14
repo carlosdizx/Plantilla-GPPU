b0719df5ca7046a26ccd155a9fb851a19cb81d6d
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-14 11:19:24 -0500

    Ya se tiene una idea de diseño de la navbar

--- src/components/NavDrawer.vue
+++ src/components/NavDrawer.vue
@@ -1,74 +1,63 @@
 <template>
-  <v-card
-    id="core-navigation-drawer"
-    absolute
-    color="transparent"
-    flat
-    :right="$vuetify.rtl"
-    mobile-break-point="960"
-    app
-    width="256"
-    v-bind="$attrs"
-    height="100%"
-  >
-    <v-navigation-drawer absolute>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>carlodiaz@umariana.edu.co</v-list-item-title>
-          <v-list-item-title>
-            <router-link to="/login">
-              <a>Registrarme/Actualizar</a>
-            </router-link>
-          </v-list-item-title>
-          <v-list-item-subtitle>Estudiante</v-list-item-subtitle>
-          <v-list-item-subtitle>
-            <router-link to="/token">
-              <a>Iniciar sesion</a>
-            </router-link>
-          </v-list-item-subtitle>
-          <v-list-item-subtitle>
-            <router-link to="/recursos">
-              <a>Recursos</a>
-            </router-link>
-          </v-list-item-subtitle>
-          <v-list-item-subtitle>
-            <a type="button">Cerrar sesion</a>
-          </v-list-item-subtitle>
-        </v-list-item-content>
-        <!--
+  <v-navigation-drawer>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>carlodiaz@umariana.edu.co</v-list-item-title>
+        <v-list-item-title>
+          <router-link to="/login">
+            <a>Registrarme/Actualizar</a>
+          </router-link>
+        </v-list-item-title>
+        <v-list-item-subtitle>Estudiante</v-list-item-subtitle>
+        <v-list-item-subtitle>
+          <router-link to="/token">
+            <a>Iniciar sesion</a>
+          </router-link>
+        </v-list-item-subtitle>
+        <v-list-item-subtitle>
+          <router-link to="/recursos">
+            <a>Recursos</a>
+          </router-link>
+        </v-list-item-subtitle>
+        <v-list-item-subtitle>
+          <a type="button">Cerrar sesion</a>
+        </v-list-item-subtitle>
+      </v-list-item-content>
+      <!--
         <v-avatar>
           <img src="../assets/estudiante.jpg" alt="John" />
         </v-avatar>
         -->
-        <router-link to="/login">
-          <v-avatar color="indigo">
-            <v-icon dark>mdi-account-circle</v-icon>
-          </v-avatar>
-        </router-link>
-        <router-link to="/login">
-          <v-avatar color="indigo">
-            <img src="@/assets/estudiante.jpg" alt="Fernando" />
-          </v-avatar>
-        </router-link>
-      </v-list-item>
+      <router-link to="/login">
+        <v-avatar color="indigo">
+          <v-icon dark>mdi-account-circle</v-icon>
+        </v-avatar>
+      </router-link>
+      <router-link to="/login">
+        <v-avatar color="indigo">
+          <img src="@/assets/estudiante.jpg" alt="Fernando" />
+        </v-avatar>
+      </router-link>
+    </v-list-item>
 
-      <v-divider dense nav></v-divider>
-      <v-list-item v-for="item in items" :key="item.title">
-        <v-row>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <router-link :to="item.link">
-            <v-row class="pt-4">
-              <v-list-item-content>
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item-content>
-            </v-row>
-          </router-link>
-        </v-row>
-      </v-list-item>
-    </v-navigation-drawer>
-  </v-card>
+    <v-divider dense nav></v-divider>
+    <v-list-item v-for="item in items" :key="item.title">
+      <!--
+      <v-row>
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <router-link :to="item.link">
+          <v-row class="pt-4">
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-row>
+        </router-link>
+      </v-row>
+      -->
+    </v-list-item>
+  </v-navigation-drawer>
 </template>
 
 <script>
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,24 +1,49 @@
 <template>
-  <v-app-bar
-    app
-    dark
-    src="@/assets/difuminado.jpg"
-  >
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-toolbar-title>GPPU</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-btn icon>
-      <v-icon>mdi-account</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>mdi-export</v-icon>
-    </v-btn>
-  </v-app-bar>
+  <v-app>
+    <v-app-bar app dark src="@/assets/difuminado.jpg">
+      <v-app-bar-nav-icon @click="menu = !menu"></v-app-bar-nav-icon>
+      <v-toolbar-title>GPPU</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="menu">
+      <v-list-item v-for="item in items" :key="item.title">
+        <v-row>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <router-link :to="item.link">
+            <v-row class="pt-4">
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-row>
+          </router-link>
+        </v-row>
+      </v-list-item>
+    </v-navigation-drawer>
+  </v-app>
 </template>
 
 <script>
+import NavDrawer from "@/components/NavDrawer";
+import { ITEMS } from "@/global/itemsNav";
 export default {
-  name: "Toolbar"
+  name: "Toolbar",
+  components: {
+    NavDrawer
+  },
+  data: () => {
+    return {
+      menu: true,
+      items: ITEMS
+    };
+  }
 };
 </script>
 
