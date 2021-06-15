611745482f7abe87b8b0032a266042c6f116e30a
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-15 13:03:24 -0500

    Validaciones para el login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,47 +10,56 @@
 			/>
 		</v-card-title>
 		<v-card-text>
-			<v-form @submit.prevent="submit">
-				<validation-provider v-slot="{ errors }" name="email" rules="required|email">
-					<v-text-field
-						v-model="userTemplate.email"
-						:error-messages="errors"
-						label="Correo electronico"
-						prepend-icon="mdi-account-circle"
-						required
+			<validation-observer ref="observer" v-slot="{ invalid }">
+				<v-form @submit.prevent="submit">
+					<validation-provider v-slot="{ errors }" name="email" rules="required|email">
+						<v-text-field
+							v-model="userTemplate.email"
+							:error-messages="errors"
+							label="Correo electronico"
+							prepend-icon="mdi-account-circle"
+							required
+						>
+						</v-text-field>
+					</validation-provider>
+					<validation-provider
+						v-slot="{ errors }"
+						name="Contraseña"
+						rules="required|min:8"
 					>
-					</v-text-field>
-				</validation-provider>
-				<validation-provider v-slot="{ errors }" name="Contraseña" rules="required|min:8">
-					<v-text-field
-						v-model="userTemplate.password"
-						type="password"
-						label="Contraseña"
-						:error-messages="errors"
-						name="input-10-1"
-						counter
-						prepend-icon="mdi-lock"
-						:type="showPassword ? 'text' : 'password'"
-						:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-						@click:append="showPassword = !showPassword"
+						<v-text-field
+							v-model="userTemplate.password"
+							type="password"
+							label="Contraseña"
+							:error-messages="errors"
+							name="input-10-1"
+							counter
+							prepend-icon="mdi-lock"
+							:type="showPassword ? 'text' : 'password'"
+							:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+							@click:append="showPassword = !showPassword"
+						>
+						</v-text-field>
+					</validation-provider>
+				</v-form>
+				<router-link class="mr-5" to="/dashboard" v-slot="{ navigate }" custom>
+					<v-btn
+						@click="navigate"
+						@keypress.enter="navigate"
+						role="link"
+						color="info"
+						type="submit"
+						:disabled="invalid"
+						>Iniciar sesion</v-btn
 					>
-					</v-text-field>
-				</validation-provider>
-			</v-form>
+				</router-link>
+				<router-link to="/forms" v-slot="{ navigate }" custom>
+					<v-btn @click="navigate" @keypress.enter="navigate" role="link" color="success"
+						>Registrarme</v-btn
+					>
+				</router-link>
+			</validation-observer>
 		</v-card-text>
-		<v-card-actions>
-			<router-link to="/dashboard" v-slot="{ navigate }" custom>
-				<v-btn @click="navigate" @keypress.enter="navigate" role="link" color="info"
-					>Iniciar sesion</v-btn
-				>
-			</router-link>
-			<router-link to="/forms" v-slot="{ navigate }" custom>
-				<v-btn @click="navigate" @keypress.enter="navigate" role="link" color="success"
-					>Registrarme</v-btn
-				>
-			</router-link>
-			<v-spacer></v-spacer>
-		</v-card-actions>
 	</v-card>
 </template>
 
@@ -63,38 +72,38 @@
 		setInteractionMode,
 	} from 'vee-validate';
 
-  setInteractionMode('eager');
+	setInteractionMode('eager');
 
-  extend('digits', {
-    ...digits,
-    message: '{_field_} needs to be {length} digits. ({_value_})',
-  });
+	extend('digits', {
+		...digits,
+		message: '{_field_} needs to be {length} digits. ({_value_})',
+	});
 
-  extend('required', {
-    ...required,
-    message: '{_field_} can not be empty',
-  });
+	extend('required', {
+		...required,
+		message: '{_field_} can not be empty',
+	});
 
-  extend('max', {
-    ...max,
-    message: '{_field_} may not be greater than {length} characters',
-  });
+	extend('max', {
+		...max,
+		message: '{_field_} may not be greater than {length} characters',
+	});
 
-  extend('min', {
-    ...min,
-    message: '{_field_} => Ingrese mas caracteres, minimo {length} ',
-  });
+	extend('min', {
+		...min,
+		message: '{_field_} => Ingrese mas caracteres, minimo {length} ',
+	});
 
-  extend('email', {
-    ...email,
-    message: 'Correo con formato incorrecto',
-  });
+	extend('email', {
+		...email,
+		message: 'Correo con formato incorrecto',
+	});
 
 	export default {
-    components: {
-      ValidationProvider,
-      ValidationObserver,
-    },
+		components: {
+			ValidationProvider,
+			ValidationObserver,
+		},
 		name: 'Login',
 		data: () => ({
 			userTemplate: {
