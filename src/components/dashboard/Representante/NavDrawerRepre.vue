749cdf73778e7310a659552f822ee520bee97857
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-16 14:45:35 -0500

    Se agrego los item de navegacion de navbar del representante

--- src/components/dashboard/Representante/NavDrawerRepre.vue
+++ src/components/dashboard/Representante/NavDrawerRepre.vue
@@ -19,31 +19,25 @@
 		<!--ITEMS DE NAVEGACION -->
 		<v-list dense nav>
 			<v-list-item v-for="item in items" :key="item.title">
-				<v-row>
-					<v-list-item-icon>
-						<v-icon>{{ item.icon }}</v-icon>
-					</v-list-item-icon>
-					<router-link :to="item.link">
-						<v-row class="pt-4">
-							<v-list-item-content>
-								<v-list-item-title>{{ item.title }}</v-list-item-title>
-							</v-list-item-content>
-						</v-row>
-					</router-link>
-				</v-row>
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+				<v-list-item-content>
+					<v-list-item-title>{{ item.title }}</v-list-item-title>
+				</v-list-item-content>
 			</v-list-item>
 		</v-list>
 	</v-navigation-drawer>
 </template>
 
 <script>
-	import { ITEMS } from '@/global/itemsNav';
+	import { ITEMS_REPRESENTANTE } from '@/global/itemsNav';
 	import { mapState } from 'vuex';
 	export default {
 		name: 'NavDrawer',
 		data() {
 			return {
-				items: ITEMS,
+				items: ITEMS_REPRESENTANTE,
 				numero: Math.floor(Math.random() * (2 - 1 + 1) + 1),
 				userTemplate: {
 					nombre: 'Carlos Ernesto Díaz Basante',
--- src/global/itemsNav.ts
+++ src/global/itemsNav.ts
@@ -1,4 +1,4 @@
-export const ITEMS = [
+export const ITEMS_ESTUDIANTE = [
 	{ title: 'Puesto de practica', icon: 'mdi-domain', link: '/' },
 	{ title: 'Bitacora', icon: 'mdi-book-arrow-up', link: '/' },
 	{ title: 'Tareas', icon: 'mdi-briefcase', link: '/' },
@@ -7,7 +7,14 @@ export const ITEMS = [
 		icon: 'mdi-google-analytics',
 		link: '/',
 	},
-	{ title: 'Mi perfil', icon: 'mdi-account', link: '/' },
-	{ title: 'Paz y salvo', icon: 'mdi-book-check', link: '/' },
+	{ title: 'Mi perfil', icon: 'mdi-briefcase', link: '/' },
+	{ title: 'Paz y salvo', icon: 'mdi-google-analytics', link: '/' },
 	{ title: 'Acerca', icon: 'mdi-information', link: '/' },
+];
+
+export const ITEMS_REPRESENTANTE = [
+	{ title: 'Responsables', icon: 'mdi-account-hard-hat', value: 1 },
+	{ title: 'Informes', icon: 'mdi-file-document', value: 2 },
+	{ title: 'Renovar convenio', icon: 'mdi-handshake', value: 3 },
+	{ title: 'Necesito ayuda', icon: 'mdi-help', value: 4 },
 ];
